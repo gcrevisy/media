de5ad12277e5d8ed729453dfbf52d6aa23fd171b
gcrevisy <gcrevisy@outlook.com>
2019-10-30 16:20:43 +0100

    correction imports

--- media-api/src/main/java/fr/gcrevisy/media/App.java
+++ media-api/src/main/java/fr/gcrevisy/media/App.java
@@ -2,6 +2,7 @@ package fr.gcrevisy.media;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "fr.alteca.media.controller")
