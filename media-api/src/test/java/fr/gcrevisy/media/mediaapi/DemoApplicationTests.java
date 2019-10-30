6cb8824be6892cda25dbf11bf42d04c91677a30d
gcrevisy <gcrevisy@outlook.com>
2019-10-30 11:14:44 +0100

    commit initial

---
+++ media-api/src/test/java/fr/gcrevisy/media/mediaapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gcrevisy.media.mediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
