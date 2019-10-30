62e83357df76feedd6ec7279af0bcae596702da7
gcrevisy <gcrevisy@outlook.com>
2019-10-30 17:39:10 +0100

    vrac

--- media-api/src/main/java/fr/gcrevisy/media/controller/FilmController.java
+++ media-api/src/main/java/fr/gcrevisy/media/controller/FilmController.java
@@ -1,10 +1,10 @@
 package fr.gcrevisy.media.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class FilmController {
 
 	@ResponseBody
