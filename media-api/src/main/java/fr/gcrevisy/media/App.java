35fdf46a8e991ecaf7da771be458b4fd525c6bb2
gcrevisy <gcrevisy@outlook.com>
2019-10-30 17:16:13 +0100

    modification controller / correction application / modification port

--- media-api/src/main/java/fr/gcrevisy/media/App.java
+++ media-api/src/main/java/fr/gcrevisy/media/App.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "fr.alteca.media.controller")
+@ComponentScan(basePackages = "fr.gcrevisy.media.controller")
 public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args); 
+		SpringApplication.run(App.class, args);
 	}
 
 }
--- media-api/src/main/java/fr/gcrevisy/media/controller/FilmController.java
+++ media-api/src/main/java/fr/gcrevisy/media/controller/FilmController.java
@@ -1,8 +1,16 @@
 package fr.gcrevisy.media.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class FilmController{
+public class FilmController {
+
+	@ResponseBody
+	@GetMapping(value = { "/getcheck" })
+	public String test() {
+		return "Alive !";
+	}
 
 }
