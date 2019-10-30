dc1e153b1e97625fd5f5723d237b884e5f2640b0
gcrevisy <gcrevisy@outlook.com>
2019-10-30 17:44:17 +0100

    correction erreur de compilation

--- media-api/src/main/java/fr/gcrevisy/media/controller/FilmController.java
+++ media-api/src/main/java/fr/gcrevisy/media/controller/FilmController.java
@@ -1,8 +1,8 @@
 package fr.gcrevisy.media.controller;
 
-import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilmController {
