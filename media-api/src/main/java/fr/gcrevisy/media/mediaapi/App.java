a9948e2e44f61a63964e73ca65f3942e7598dcb7
gcrevisy <gcrevisy@outlook.com>
2019-10-30 16:03:29 +0100

    refactoring

--- media-api/src/main/java/fr/gcrevisy/media/mediaapi/App.java
+++ media-api/src/main/java/fr/gcrevisy/media/mediaapi/App.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@ComponentScan(basePackages = "fr.alteca.media.controller")
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(App.class, args); 
 	}
 
 }
