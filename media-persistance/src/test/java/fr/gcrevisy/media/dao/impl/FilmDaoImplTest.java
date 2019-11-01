50d59e043d01f77c0db734afe0afa34ffa430934
gcrevisy <gcrevisy@outlook.com>
2019-11-01 17:01:55 +0100

    test

---
+++ media-persistance/src/test/java/fr/gcrevisy/media/dao/impl/FilmDaoImplTest.java
@@ -0,0 +1,12 @@
+package fr.gcrevisy.media.dao.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FilmDaoImplTest {
+
+    @Test
+    public void totoTest() {
+        Assert.assertTrue(true);
+    }
+}
