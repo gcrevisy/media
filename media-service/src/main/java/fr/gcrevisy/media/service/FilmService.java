6bc2d1db36d1d9577e07de9747238bba669da4aa
gcrevisy <gcrevisy@outlook.com>
2019-11-01 19:50:33 +0100

    ajout TU

--- media-service/src/main/java/fr/gcrevisy/media/service/FilmService.java
+++ media-service/src/main/java/fr/gcrevisy/media/service/FilmService.java
@@ -1,7 +1,20 @@
 package fr.gcrevisy.media.service;
 
+import java.util.List;
+
+import fr.gcrevisy.media.exception.TechnicalException;
+import fr.gcrevisy.media.model.metier.Film;
+
 public interface FilmService {
 
-    
+    void delete(String id) throws TechnicalException;
+
+    void delete(Film item) throws TechnicalException;
+
+    Film saveOrUpdate(Film item) throws TechnicalException;
+
+    List<Film> getAll();
+
+    Film getById(String id) throws TechnicalException;
 
 }
--- media-service/src/test/java/fr/gcrevisy/media/service/impl/FilmServiceImplTest.java
+++ media-service/src/test/java/fr/gcrevisy/media/service/impl/FilmServiceImplTest.java
@@ -2,10 +2,33 @@ package fr.gcrevisy.media.service.impl;
 
 import org.junit.Test;
 
+import fr.gcrevisy.media.exception.TechnicalException;
+import fr.gcrevisy.media.model.metier.Film;
+import fr.gcrevisy.media.service.FilmService;
+
 public class FilmServiceImplTest {
 
-    @Test
-    public void test() {
-        System.out.println("TOTO");
+    @Test(expected = TechnicalException.class)
+    public void deleteItemNull() throws TechnicalException {
+        FilmService service = new FilmServiceImpl();
+        service.delete((Film) null);
+    }
+
+    @Test(expected = TechnicalException.class)
+    public void deleteIdNull() throws TechnicalException {
+        FilmService service = new FilmServiceImpl();
+        service.delete((String) null);
+    }
+
+    @Test(expected = TechnicalException.class)
+    public void saveOrUpdateItemNull() throws TechnicalException {
+        FilmService service = new FilmServiceImpl();
+        service.saveOrUpdate(null);
+    }
+
+    @Test(expected = TechnicalException.class)
+    public void getByIdIdNull() throws TechnicalException {
+        FilmService service = new FilmServiceImpl();
+        service.getById(null);
     }
 }
